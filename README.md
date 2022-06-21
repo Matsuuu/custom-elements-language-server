1ee60dfdb1cb93637139a1f2ef1f91c0260eab6f
Matsuuu <huhta.matias@gmail.com>
2022-06-21 22:01:48 +0300

    PoC Server code stub

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # custom-elements-language-server
+
+
+Helpful Sources:
+
+- https://code.visualstudio.com/api/language-extensions/language-server-extension-guide
+
+- https://github.com/microsoft/vscode-extension-samples/blob/main/lsp-sample/server/src/server.ts
---
+++ lib/server/src/server.ts
@@ -0,0 +1,219 @@
+import {
+    CompletionItem,
+    CompletionItemKind,
+    createConnection,
+    Diagnostic,
+    DiagnosticSeverity,
+    DidChangeConfigurationNotification,
+    DidChangeConfigurationParams,
+    InitializeParams,
+    InitializeResult,
+    NotificationHandler,
+    ProposedFeatures,
+    TextDocumentPositionParams,
+    TextDocuments,
+    TextDocumentSyncKind,
+} from "vscode-languageserver/node";
+
+import { TextDocument } from "vscode-languageserver-textdocument";
+
+interface LanguageServerSettings { }
+
+/**
+ * ==============================================================================================0
+ *
+ * Quite a lot of this file's code is set up following the extension guide from visual studio code docs.
+ * This  is just to provide a PoC before actually going head deep into development
+ *
+ * ==============================================================================================0
+ */
+
+const connection = createConnection(ProposedFeatures.all);
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+let defaultSettings: LanguageServerSettings = {};
+let globalSettings: LanguageServerSettings = defaultSettings;
+
+const documentSettings = new Map<string, LanguageServerSettings>();
+// Only keep settings for open documents
+documents.onDidClose((e) => {
+    documentSettings.delete(e.document.uri);
+});
+
+let hasConfigurationCapability: boolean = false;
+let hasWorkspaceFolderCapability: boolean = false;
+let hasDiagnosticRelatedInformationCapability: boolean = false;
+
+connection.onInitialize(onInitialize);
+connection.onInitialized(onInitialized);
+connection.onDidChangeConfiguration(onDidChangeConfiguration);
+connection.onDidChangeWatchedFiles((_change) => {
+    // Monitored files have change in VS Code
+    connection.console.log("We received a file change event");
+});
+// This handler provides the initial list of the completion items.
+connection.onCompletion(
+    (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+        // The pass parameter contains the position of the text document in
+        // which code complete got requested. For the example we ignore this
+        // info and always provide the same completion items.
+        return [
+            {
+                label: "TypeScript",
+                kind: CompletionItemKind.Text,
+                data: 1,
+            },
+            {
+                label: "JavaScript",
+                kind: CompletionItemKind.Text,
+                data: 2,
+            },
+        ];
+    }
+);
+// This handler resolves additional information for the item selected in
+// the completion list.
+connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
+    if (item.data === 1) {
+        item.detail = "TypeScript details";
+        item.documentation = "TypeScript documentation";
+    } else if (item.data === 2) {
+        item.detail = "JavaScript details";
+        item.documentation = "JavaScript documentation";
+    }
+    return item;
+});
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// Listen on the connection
+connection.listen();
+
+function onInitialize(params: InitializeParams) {
+    let capabilities = params.capabilities;
+
+    // Does the client support the `workspace/configuration` request?
+    // If not, we fall back using global settings.
+    hasConfigurationCapability = !!(
+        capabilities.workspace && !!capabilities.workspace.configuration
+    );
+    hasWorkspaceFolderCapability = !!(
+        capabilities.workspace && !!capabilities.workspace.workspaceFolders
+    );
+    hasDiagnosticRelatedInformationCapability = !!(
+        capabilities.textDocument &&
+        capabilities.textDocument.publishDiagnostics &&
+        capabilities.textDocument.publishDiagnostics.relatedInformation
+    );
+
+    const result: InitializeResult = {
+        capabilities: {
+            textDocumentSync: TextDocumentSyncKind.Incremental,
+            // Tell the client that this server supports code completion.
+            completionProvider: {
+                resolveProvider: true,
+            },
+        },
+    };
+    if (hasWorkspaceFolderCapability) {
+        result.capabilities.workspace = {
+            workspaceFolders: {
+                supported: true,
+            },
+        };
+    }
+    return result;
+}
+
+function onInitialized() {
+    if (hasConfigurationCapability) {
+        // Register for all configuration changes.
+        connection.client.register(
+            DidChangeConfigurationNotification.type,
+            undefined
+        );
+    }
+    if (hasWorkspaceFolderCapability) {
+        connection.workspace.onDidChangeWorkspaceFolders((_event) => {
+            connection.console.log("Workspace folder change event received.");
+        });
+    }
+}
+
+function onDidChangeConfiguration(change: DidChangeConfigurationParams) {
+    if (hasConfigurationCapability) {
+        // Reset all cached document settings
+        documentSettings.clear();
+    } else {
+        globalSettings = <LanguageServerSettings>(
+            (change.settings.languageServerExample || defaultSettings)
+        );
+    }
+
+    // Revalidate all open text documents
+    documents.all().forEach(validateTextDocument);
+}
+
+async function getDocumentSettings(
+    resource: string
+): Promise<LanguageServerSettings> {
+    if (!hasConfigurationCapability) {
+        return Promise.resolve(globalSettings);
+    }
+    let result = documentSettings.get(resource);
+    if (!result) {
+        result = connection.workspace.getConfiguration({
+            scopeUri: resource,
+            section: "customElementsLanguageServer",
+        });
+        documentSettings.set(resource, result);
+    }
+    return result;
+}
+
+async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+    // In this simple example we get the settings for every validate run.
+    let settings = await getDocumentSettings(textDocument.uri);
+
+    // The validator creates diagnostics for all uppercase words length 2 and more
+    let text = textDocument.getText();
+    let pattern = /\b[A-Z]{2,}\b/g;
+    let m: RegExpExecArray | null;
+
+    let diagnostics: Diagnostic[] = [];
+    while ((m = pattern.exec(text))) {
+        let diagnostic: Diagnostic = {
+            severity: DiagnosticSeverity.Warning,
+            range: {
+                start: textDocument.positionAt(m.index),
+                end: textDocument.positionAt(m.index + m[0].length),
+            },
+            message: `${m[0]} is all uppercase.`,
+            source: "ex",
+        };
+        if (hasDiagnosticRelatedInformationCapability) {
+            diagnostic.relatedInformation = [
+                {
+                    location: {
+                        uri: textDocument.uri,
+                        range: Object.assign({}, diagnostic.range),
+                    },
+                    message: "Spelling matters",
+                },
+                {
+                    location: {
+                        uri: textDocument.uri,
+                        range: Object.assign({}, diagnostic.range),
+                    },
+                    message: "Particularly for names",
+                },
+            ];
+        }
+        diagnostics.push(diagnostic);
+    }
+
+    // Send the computed diagnostics to VS Code.
+    connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+}
