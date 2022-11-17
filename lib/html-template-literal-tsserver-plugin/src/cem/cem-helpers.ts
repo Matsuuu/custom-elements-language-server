ffdc1cd72361b7ff7411785e6a2f4acec4563ad8
Matsuuu <huhta.matias@gmail.com>
2022-11-17 21:31:22 +0200

    Add cem fetching and caching PoC

--- lib/html-template-literal-tsserver-plugin/src/cem/cem-helpers.ts
+++ lib/html-template-literal-tsserver-plugin/src/cem/cem-helpers.ts
@@ -12,10 +12,15 @@ export function findClassForTagName(manifest: Package, tagName: string) {
     if (!declarationExport) return undefined;
 
     const declaration = declarationExport.declaration;
-    const className = declaration.name;
     const classPath = declaration.module;
 
-    return undefined;
+    if (!classPath) return undefined;
+
+    const mod = findModuleByPath(manifest, classPath);
+    if (!mod) return undefined;
+
+    CEMCache.set(tagName, mod);
+    return mod;
 }
 
 export function moduleHasCustomElementExportByName(mod: Module, tagName: string) {
@@ -24,4 +29,12 @@ export function moduleHasCustomElementExportByName(mod: Module, tagName: string)
 
 export function exportHasCustomElementExportByName(modExport: Export, tagName: string) {
     return modExport.kind === "custom-element-definition" && modExport.name === tagName;
+}
+
+export function findModuleByPath(manifest: Package, path: string) {
+    return manifest.modules.find(mod => modulePathEquals(mod, path));
+}
+
+export function modulePathEquals(mod: Module, path: string) {
+    return mod.path === path;
 }
