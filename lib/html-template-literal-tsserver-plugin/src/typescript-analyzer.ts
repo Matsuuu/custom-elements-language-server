2a2707bfc163dc81de14bfc3ed8dc4a44ec3750a
Matsuuu <huhta.matias@gmail.com>
2022-12-06 23:07:17 +0200

    Set parent nodes on AST traversal

--- lib/html-template-literal-tsserver-plugin/src/typescript-analyzer.ts
+++ lib/html-template-literal-tsserver-plugin/src/typescript-analyzer.ts
@@ -1,22 +1,17 @@
 import { JavaScriptModule } from "custom-elements-manifest";
 import ts from "typescript";
 
+const ZERO_TEXT_SPAN = ts.createTextSpan(0, 0);
+
 export function getClassDefinitionTextSpan(mod: JavaScriptModule, className: string, basePath: string): ts.TextSpan {
-    const classPath = basePath + "/" + mod.path;
-    const program = ts.createProgram({
-        rootNames: [classPath],
-        options: {}
-    });
-
-    const sourceFile = program.getSourceFile(classPath);
-
+    const sourceFile = getSourceFile(basePath, mod.path);
     if (!sourceFile) {
-        return ts.createTextSpan(0, 0);
+        return ZERO_TEXT_SPAN;
     }
 
     const classIdentifier = findClassIdentifierByName(sourceFile, className);
     if (!classIdentifier) {
-        return ts.createTextSpan(0, 0);
+        return ZERO_TEXT_SPAN;
     }
 
     return {
@@ -25,10 +20,35 @@ export function getClassDefinitionTextSpan(mod: JavaScriptModule, className: str
     }
 }
 
+export function getAttributeDefinitionTextSpan(mod: JavaScriptModule, attributeName: string, basePath: string): ts.TextSpan {
+    const sourceFile = getSourceFile(basePath, mod.path);
+    if (!sourceFile) {
+        return ts.createTextSpan(0, 0);
+    }
+
+    const propertyIdentifier = findAttributeIdentifierByName(sourceFile, attributeName);
+
+    return ZERO_TEXT_SPAN;
+}
+
+function getSourceFile(basePath: string, classPath: string) {
+    // TODO: Make some of these static / final ?
+    const fullClassPath = basePath + "/" + classPath;
+    const compilerHost = ts.createCompilerHost({}, true);
+    const program = ts.createProgram({
+        rootNames: [fullClassPath],
+        options: {},
+        host: compilerHost
+    });
+
+    return program.getSourceFile(classPath);
+}
+
 function findClassIdentifierByName(sourceFile: ts.SourceFile, className: string): ts.Identifier | undefined {
     let foundClassIdentifier: ts.Node | undefined = undefined;
     function findClassIdentifier(node: ts.Node) {
         if (ts.isIdentifier(node) && node.escapedText === className) {
+            debugger;
             foundClassIdentifier = node;
             return;
         }
@@ -38,3 +58,9 @@ function findClassIdentifierByName(sourceFile: ts.SourceFile, className: string)
 
     return foundClassIdentifier;
 }
+
+function findAttributeIdentifierByName(sourceFile: ts.SourceFile, className: string): ts.Identifier | undefined {
+
+    return undefined;
+}
+
