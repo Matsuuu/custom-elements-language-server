66beab8c9357ed9dc3899000383da23b123defb6
Matsuuu <huhta.matias@gmail.com>
2022-12-16 17:32:08 +0200

    Some general setup code for quickinfo

--- lib/html-template-literal-tsserver-plugin/src/handlers/quickinfo.ts
+++ lib/html-template-literal-tsserver-plugin/src/handlers/quickinfo.ts
@@ -1,6 +1,7 @@
 import { TemplateContext } from "typescript-template-language-service-decorator";
 import tss from "typescript/lib/tsserverlibrary.js";
 import { LanguageService as HtmlLanguageService } from "vscode-html-languageservice";
+import { findClassForTagName } from "../cem/cem-helpers.js";
 import { getLatestCEM } from "../cem/cem-instance.js";
 import { isAttributeNameAction, isEndTagAction, isEventNameAction, isPropertyNameAction, isTagAction, resolveActionContext } from "../completion-context.js";
 import { getProjectBasePath } from "../template-context.js";
@@ -11,7 +12,16 @@ export function getQuickInfo(context: TemplateContext, position: tss.LineAndChar
     const actionContext = resolveActionContext(htmlLanguageService, context, position);
     const cem = getLatestCEM();
 
-    const sourceFile = getSourceFile(basePath, mod.path);
+    if (!cem) {
+        return undefined;
+    }
+
+    const matchingClass = findClassForTagName(cem, actionContext.tagName);
+    if (!matchingClass) {
+        return undefined;
+    }
+
+    const sourceFile = getSourceFile(basePath, matchingClass.path);
     if (!sourceFile) {
         return undefined;
     }
