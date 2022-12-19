24183e656d8cf82ff53b9c33e0c9b02999d6eda4
Matsuuu <huhta.matias@gmail.com>
2022-12-19 22:52:23 +0200

    Update formatters

--- lib/html-template-literal-tsserver-plugin/src/handlers/quickinfo.ts
+++ lib/html-template-literal-tsserver-plugin/src/handlers/quickinfo.ts
@@ -1,11 +1,11 @@
-import { TemplateContext } from "typescript-template-language-service-decorator";
-import tss from "typescript/lib/tsserverlibrary.js";
-import { LanguageService as HtmlLanguageService } from "vscode-html-languageservice";
-import { findClassForTagName } from "../cem/cem-helpers.js";
-import { getLatestCEM } from "../cem/cem-instance.js";
-import { isAttributeNameAction, isEndTagAction, isEventNameAction, isPropertyNameAction, isTagAction, resolveActionContext } from "../completion-context.js";
-import { getProjectBasePath } from "../template-context.js";
-import { getSourceFile } from "../typescript-analyzer.js";
+import { TemplateContext } from 'typescript-template-language-service-decorator';
+import tss from 'typescript/lib/tsserverlibrary.js';
+import { LanguageService as HtmlLanguageService } from 'vscode-html-languageservice';
+import { findClassForTagName } from '../cem/cem-helpers.js';
+import { getLatestCEM } from '../cem/cem-instance.js';
+import { isAttributeNameAction, isEndTagAction, isEventNameAction, isPropertyNameAction, isTagAction, resolveActionContext } from '../completion-context.js';
+import { getProjectBasePath } from '../template-context.js';
+import { getSourceFile } from '../typescript-analyzer.js';
 
 export function getQuickInfo(context: TemplateContext, position: tss.LineAndCharacter, htmlLanguageService: HtmlLanguageService) {
     const basePath = getProjectBasePath(context);
@@ -27,19 +27,15 @@ export function getQuickInfo(context: TemplateContext, position: tss.LineAndChar
     }
 
     if (isTagAction(actionContext) || isEndTagAction(actionContext)) {
-
     }
 
     if (isAttributeNameAction(actionContext)) {
-
     }
 
     if (isPropertyNameAction(actionContext)) {
-
     }
 
     if (isEventNameAction(actionContext)) {
-
     }
 
     return undefined;
