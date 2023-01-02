ed177a5cd0822a6dfb3cdce882a6da50e5a1e53c
Matsuuu <huhta.matias@gmail.com>
2023-01-02 22:28:24 +0200

    Allow js files in source file parsing

--- lib/html-template-literal-tsserver-plugin/src/ts/sourcefile.ts
+++ lib/html-template-literal-tsserver-plugin/src/ts/sourcefile.ts
@@ -3,11 +3,13 @@ import ts from "typescript";
 export function getSourceFile(basePath: string, classPath: string) {
     // TODO: Make some of these static / final ?
     // TODO: Yeah we need to really cache this shit
-    const fullClassPath = basePath + "/" + classPath;
+    const fullClassPath = [basePath, classPath].filter(p => p.trim().length > 0).join("/");
     const compilerHost = ts.createCompilerHost({}, true);
     const program = ts.createProgram({
         rootNames: [fullClassPath],
-        options: {},
+        options: {
+            allowJs: true
+        },
         host: compilerHost,
     });
     // NOTE: this makes everything slow as shit
