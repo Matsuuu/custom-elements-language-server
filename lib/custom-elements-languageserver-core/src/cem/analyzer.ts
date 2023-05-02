79530428e9d039aceaa33e58193624f2c8db1d6d
Matsuuu <huhta.matias@gmail.com>
2023-05-02 20:14:29 +0300

    Write custom elements cache PoC

--- lib/custom-elements-languageserver-core/src/cem/analyzer.ts
+++ lib/custom-elements-languageserver-core/src/cem/analyzer.ts
@@ -4,11 +4,14 @@ import { create, ts } from "@custom-elements-manifest/analyzer";
 import { litPlugin } from "@custom-elements-manifest/analyzer/src/features/framework-plugins/lit/lit.js";
 
 import tss from "typescript/lib/tsserverlibrary.js";
+import fs from "fs";
+import path from "path";
 
 export function analyzeLocalProject(project: tss.server.Project) {
 
     console.log("Building manifest");
 
+    const basePath = project.getCurrentDirectory();
     const rootFiles = project.getRootFiles();
     const sourceFiles = rootFiles
         // @ts-ignore
@@ -30,9 +33,14 @@ export function analyzeLocalProject(project: tss.server.Project) {
         context: { dev: true }
     });
 
+    cacheCurrentCEM(basePath, manifest);
     console.log("Building manifest done");
 
-    debugger;
-
     return manifest;
+}
+
+function cacheCurrentCEM(projectPath: string, manifest: unknown) {
+    const cachePath = path.resolve(projectPath, ".custom-elements-language-server");
+    fs.mkdirSync(cachePath, { recursive: true });
+    fs.writeFileSync(path.resolve(cachePath, "custom-elements.json"), JSON.stringify(manifest), "utf8");
 }
