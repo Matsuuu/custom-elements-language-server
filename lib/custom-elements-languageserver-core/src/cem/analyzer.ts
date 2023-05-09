c50a28d3e3a9a8517b93003dacc944e8f1f61208
Matsuuu <huhta.matias@gmail.com>
2023-05-09 18:42:47 +0300

    Normalize cached CEM paths

--- lib/custom-elements-languageserver-core/src/cem/analyzer.ts
+++ lib/custom-elements-languageserver-core/src/cem/analyzer.ts
@@ -1,11 +1,17 @@
-// @ts-ignore
+// @ts-expect-error
 import { create, ts } from "@custom-elements-manifest/analyzer";
-// @ts-ignore
+// @ts-expect-error
 import { litPlugin } from "@custom-elements-manifest/analyzer/src/features/framework-plugins/lit/lit.js";
-
+// @ts-expect-error
+import { Package } from "custom-elements-manifest";
+// TODO: Can we fix these imports?
 import tss from "typescript/lib/tsserverlibrary.js";
 import fs from "fs";
 import path from "path";
+
+// Pathing to ${projectPath}/node_modules/.cache/custom-elements-language-server
+const CEM_CACHE_DIR = "/node_modules/.cache/custom-elements-language-server";
+const CEM_CACHE_NAME = "custom-elements.json";
 
 export function analyzeLocalProject(project: tss.server.Project) {
 
@@ -27,20 +33,36 @@ export function analyzeLocalProject(project: tss.server.Project) {
         )
     })
 
-    const manifest = create({
+    const manifest: Package = create({
         modules: modifiedSourceFiles,
         plugins: [...litPlugin()],
         context: { dev: true }
     });
 
-    cacheCurrentCEM(basePath, manifest);
-    console.log("Building manifest done");
+    normalizeManifest(manifest, basePath);
+
+    console.log("Building manifest done, writing to file.");
+    console.log("Manifest size: ", JSON.stringify(manifest).length);
+    const savePath = cacheCurrentCEM(basePath, manifest);
+    console.log("Manifest file wroitten to ", savePath);
 
     return manifest;
 }
 
-function cacheCurrentCEM(projectPath: string, manifest: unknown) {
-    const cachePath = path.resolve(projectPath, ".custom-elements-language-server");
-    fs.mkdirSync(cachePath, { recursive: true });
-    fs.writeFileSync(path.resolve(cachePath, "custom-elements.json"), JSON.stringify(manifest), "utf8");
+function normalizeManifest(manifest: Package, basePath: string) {
+    manifest.modules?.forEach((mod) => {
+        mod.path = mod.path.replace(basePath + "/", "");
+    });
+}
+
+function cacheCurrentCEM(projectPath: string, manifest: Package) {
+    const cachePath = path.join(projectPath, CEM_CACHE_DIR);
+    if (!fs.existsSync(cachePath)) {
+        console.log("Creating cache path ", cachePath);
+        fs.mkdirSync(cachePath, { recursive: true });
+    }
+    const savePath = path.resolve(cachePath, CEM_CACHE_NAME);
+    fs.writeFileSync(savePath, JSON.stringify(manifest), "utf8");
+
+    return savePath;
 }
