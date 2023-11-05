a1723313136b5b2133586ec704c01b0ebf7b01ad
Matsuuu <huhta.matias@gmail.com>
2023-11-05 17:48:20 +0200

    Update solid example

--- lib/server/src/handlers/definition.ts
+++ lib/server/src/handlers/definition.ts
@@ -5,6 +5,7 @@ import { documents } from "../text-documents";
 import { documentSpanToLocation, textDocumentDataToUsableData } from "../transformers";
 import { Handler, isJavascriptFile } from "./handler";
 import { createCustomElementsLanguageServiceRequest } from "../language-services/request";
+import { generateLanguageServiceQueryData } from "./handler-helper";
 
 export const DefinitionHandler: Handler<DefinitionParams, Location[] | undefined> = {
     handle: (definitionParams) => {
@@ -16,8 +17,14 @@ export const DefinitionHandler: Handler<DefinitionParams, Location[] | undefined
     },
     onJavascriptFile: (definitionParams) => {
         const usableData = textDocumentDataToUsableData(documents, definitionParams);
+        const queryData = generateLanguageServiceQueryData(usableData, definitionParams);
         const languageService = getLanguageService(usableData.fileName, usableData.fileContent);
         const definitions = languageService?.getDefinitionAtPosition(usableData.fileName, usableData.position);
+        if (definitions === undefined) {
+            if (!queryData.isValid) {
+                return undefined;
+            }
+        }
 
         const definitionLocations = definitions?.map(documentSpanToLocation) ?? [];
         return definitionLocations;
---
+++ usage-testing-project/src/solid.jsx
@@ -0,0 +1,16 @@
+import { createSignal, onCleanup } from "solid-js";
+import { render } from "solid-js/web";
+
+const CountingComponent = () => {
+	const [count, setCount] = createSignal(0);
+	const interval = setInterval(
+		() => setCount(c => c + 1),
+		1000
+	);
+	onCleanup(() => clearInterval(interval));
+  return <div>
+    <p>Count value is {count()}</p>
+  </div>;
+};
+
+render(() => <CountingComponent />, document.getElementById("app"));
