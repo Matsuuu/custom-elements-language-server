335b0d526016ef6b6eb4886fe5cbd5e2ce613d4b
Matsuuu <huhta.matias@gmail.com>
2023-11-05 17:50:52 +0200

    Restore react example

--- usage-testing-project/src/react.jsx
+++ usage-testing-project/src/react.jsx
@@ -1,54 +1,21 @@
-export default ({ appName, token, handleSetPage, tab, store }) => {
+import React from "react";
+
+function MyButton() {
   return (
-    <div class="home-page">
-      <div class="banner">
-        <div class="container">
-          <h1 class="logo-font" textContent={appName} />
-          <p>A place to share your knowledge.</p>
-        </div>
-      </div>
+    <button>
+      I'm a button
 
-      <div class="container page">
-        <div class="row">
-          <div class="col-md-9">
-            <div class="feed-toggle">
-              <ul class="nav nav-pills outline-active">
-                {token && (
-                  <li class="nav-item">
-                  </li>
-                )}
-                <li class="nav-item">
-                </li>
-                <Show when={tab() !== "all" && tab() !== "feed"}>
-                  <li class="nav-item">
-                    <a href="" class="nav-link active">
-                      <i class="ion-pound" /> {tab()}
-                    </a>
-                  </li>
-                </Show>
-              </ul>
-            </div>
+      <example-project></example-project>
 
-          </div>
+    </button>
+  );
+}
 
-          <div class="col-md-3">
-            <div class="sidebar">
-              <p>Popular Tags</p>
-              <Suspense fallback="Loading tags...">
-                <div class="tag-list">
-                  <For each={store.tags}>
-                    {tag => (
-                      <a href={`#/?tab=${tag}`} class="tag-pill tag-default">
-                        {tag}
-                      </a>
-                    )}
-                  </For>
-                </div>
-              </Suspense>
-            </div>
-          </div>
-        </div>
-      </div>
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
     </div>
   );
-};
+}
