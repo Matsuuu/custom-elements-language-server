caa39c8237a2a610481da767ad372df8320e5cae
Matsuuu <huhta.matias@gmail.com>
2023-11-09 19:51:01 +0200

    IntelliJ Plugin base created

---
+++ lib/client/intellij/src/main/kotlin/com/github/matsuuu/ideacelsplugin/services/MyProjectService.kt
@@ -0,0 +1,17 @@
+package com.github.matsuuu.ideacelsplugin.services
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.project.Project
+import com.github.matsuuu.ideacelsplugin.MyBundle
+
+@Service(Service.Level.PROJECT)
+class MyProjectService(project: Project) {
+
+    init {
+        thisLogger().info(MyBundle.message("projectService", project.name))
+        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+    }
+
+    fun getRandomNumber() = (1..100).random()
+}
